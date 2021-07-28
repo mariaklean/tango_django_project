efa6edcd5b86873ce1d706859aed32a0e71551de
Maria <makleanthous@gmail.com>
2021-07-28 13:52:18 +0300

    Chapter 4 until 4.3

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='http://127.0.0.1:8000/rango/about/'>About</a>")
+    conext_dir={'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html',context=conext_dir)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='http://127.0.0.1:8000/'>Index</a>")
