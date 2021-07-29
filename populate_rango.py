42a61d2724354908443025266bef1e0ae1cb8193
Maria <makleanthous@gmail.com>
2021-07-29 15:39:35 +0300

    Finish Chapter 6 with excercises

--- populate_rango.py
+++ populate_rango.py
@@ -10,25 +10,33 @@ def populate():
 
 	python_pages = [
 		{"title": "Official Python Tutorial",
-		"url":"http://docs.python.org/3/tutorial/"},
+		"url":"http://docs.python.org/3/tutorial/",
+		"views":55},
 		{"title":"How to Think like a Computer Scientist",
-		"url":"http://www.greenteapress.com/thinkpython/"},
+		"url":"http://www.greenteapress.com/thinkpython/",
+		"views":12},
 		{"title":"Learn Python in 10 Minutes",
-		"url":"http://www.korokithakis.net/tutorials/python/"} ]
+		"url":"http://www.korokithakis.net/tutorials/python/",
+		"views":63} ]
 
 	django_pages = [
 		{"title":"Official Django Tutorial",
-		"url":"https://docs.djangoproject.com/en/2.1/intro/tutorial01/"},
+		"url":"https://docs.djangoproject.com/en/2.1/intro/tutorial01/",
+		"views":20},
 		{"title":"Django Rocks",
-		"url":"http://www.djangorocks.com/"},
+		"url":"http://www.djangorocks.com/",
+		"views":25},
 		{"title":"How to Tango with Django",
-		"url":"http://www.tangowithdjango.com/"} ]
+		"url":"http://www.tangowithdjango.com/",
+		"views":27} ]
 
 	other_pages = [
 		{"title":"Bottle",
-		"url":"http://bottlepy.org/docs/dev/"},
+		"url":"http://bottlepy.org/docs/dev/",
+		"views":80},
 		{"title":"Flask",
-		"url":"http://flask.pocoo.org"} ]
+		"url":"http://flask.pocoo.org",
+		"views":75} ]
 
 	cats = {"Python": {"pages": python_pages, "views": 128,"likes":64},
 		"Django": {"pages": django_pages,"views": 64,"likes":32},
@@ -38,15 +46,15 @@ def populate():
 	for cat, cat_data in cats.items():
 		c = add_cat(cat, cat_data["views"], cat_data["likes"])
 		for p in cat_data["pages"]:
-			add_page(c, p["title"], p["url"])
+			add_page(c, p["title"], p["url"], p["views"])
 
 	# Print out the categories we have added.
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
 			print(f'- {c}: {p}')
 
-def add_page(cat, title, url, views=0):
-	p = Page.objects.get_or_create(category=cat, title=title)[0]
+def add_page(cat, title, url, views):
+	p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
 	p.url=url
 	p.views=views
 	p.save()
--- rango/views.py
+++ rango/views.py
@@ -5,12 +5,14 @@ from rango.models import Page
 from django.http import HttpResponse
 
 def index(request):
-   # conext_dir={'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # conext_dir={'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     # return render(request, 'rango/index.html',context=conext_dir)
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
+    context_dict = {'categories': category_list, 'pages': page_list}
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
